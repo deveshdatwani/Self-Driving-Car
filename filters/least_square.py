9e1bc0f02ee1be9ed057c814a26b5467aac995ea
devesh <datwanidevesh@gmail.com>
2020-12-29 23:42:28 +0530

    least squares

--- filters/least_square.py
+++ filters/least_square.py
@@ -1,19 +1,44 @@
 import math
 import numpy as np
 from numpy.linalg import inv
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.cleanup()
+
+# Set up pins
+TRIG = 12
+ECHO = 11
+GPIO.setup(TRIG,GPIO.OUT)
+GPIO.setup(ECHO,GPIO.IN)
+print('Setting up pins')
+distance = []
 
 # Calculate a batch of 200 readings
-
-for i in range(200):
-    a = 1
-
-
-
+for i in range(10):
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(TRIG,GPIO.OUT)
+    GPIO.setup(ECHO,GPIO.IN)
+    GPIO.output(TRIG,True)
+    time.sleep(1)
+    GPIO.output(TRIG,False)
+    while GPIO.input(11) == 0:
+        print('waiting for signal')
+        start = time.time()
+    while GPIO.input(11) == 1:
+        end = time.time()
+        print(i)
+        break
+    tl = end - start
+    distance.append(tl/0.000058)
+    GPIO.cleanup()
+    
 # Run least square method on the 200 batch readings 
 # The model is 
 # distance  = H (jacobina matrix) x x (parameter) + e (error)  
 
-distance = np.ones((200,1),dtype="float")
-H = np.ones((200,1),dtype="float")
+#distance = np.ones((200,1),dtype="float")
+H = np.ones((10,1),dtype="float")
 x_hat = inv(H.T.dot(H)).dot(H.T.dot(distance))
 print('The sitance is %s', x_hat)
